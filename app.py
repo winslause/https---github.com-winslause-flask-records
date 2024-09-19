ce1a7e842b50706342eda0f58de3198c27f6e1ed
wenslause <wenbusale383@gmail.com>
2024-09-19 10:42:48 +0300

    first

---
+++ app.py
@@ -0,0 +1,76 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_sqlalchemy import SQLAlchemy
+
+
+
+app =Flask(__name__)
+app.secret_key = 'wenny'
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root@localhost/crud"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
+class Data(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+    phone = db.Column(db.String(100))
+    
+    def __init__(self, name, email, phone):
+        self.name = name
+        self.email = email
+        self.phone = phone
+        
+        
+        
+with app.app_context():
+    db.create_all()
+
+@app.route('/')
+def index():
+    all_data = Data.query.all()
+    return render_template("index.html", employees = all_data) 
+
+
+@app.route("/insert", methods=['POST'])
+def insert():
+    if request.method == "POST":
+        name = request.form["name"]
+        email = request.form["email"]
+        phone= request.form["phone"]
+        
+        my_data = Data(name,email,phone)
+        db.session.add(my_data)
+        db.session.commit()
+        
+        flash("Employee inserted successfully")
+        
+        return redirect(url_for("index"))
+    
+    
+@app.route("/update", methods=["GET", "POST"])
+def update():
+    if request.method == 'POST':
+        my_data = Data.query.get(request.form.get("id"))
+        my_data.name = request.form["name"]
+        my_data.email = request.form["email"]
+        my_data.phone = request.form["phone"]
+        
+        db.session.commit()
+        
+        flash("Employee updated successfully")
+        return redirect(url_for("index"))
+    
+    
+@app.route("/delete/<id>/", methods=["GET", "POST"])
+def delete(id):
+    my_data = Data.query.get(id)
+    
+    db.session.delete(my_data)
+    db.session.commit()
+    flash("Employee Deleted Succesfully")
+    return redirect(url_for("index"))
+        
+    
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ crud.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 19, 2024 at 09:23 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `crud`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `data`
+--
+
+CREATE TABLE `data` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `phone` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `data`
+--
+
+INSERT INTO `data` (`id`, `name`, `email`, `phone`) VALUES
+(6, 'winslause busale shioso', 'wenbusale383@gmail.com', '0769525570'),
+(7, 'winslause busale shioso', 'wenbusale383@gmail.com', '0769525570'),
+(8, 'winslause busale shioso', 'wenbusale383@gmail.com', '0769525570'),
+(9, 'winslause busale shioso', 'wenbusale383@gmail.com', '0769525570'),
+(10, 'winslause busale shioso', 'wenbusale383@gmail.com', '0769525570');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `data`
+--
+ALTER TABLE `data`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `data`
+--
+ALTER TABLE `data`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
